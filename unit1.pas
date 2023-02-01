805893dab784e5c5fe8eba8d5a56b50785c3317d
VadoVates <121289099+VadoVates@users.noreply.github.com>
2023-02-01 21:55:25 +0100

    upload do wersji z okienkiem

--- unit1.pas
+++ unit1.pas
@@ -12,6 +12,8 @@ type
   { TForm_Main }
 
   TForm_Main = class(TForm)
+    Button_Info: TButton;
+    Button_Lista: TButton;
     Button_Oblicz: TButton;
     Button_Koniec: TButton;
     CheckGroup_Opcje: TCheckGroup;
@@ -19,8 +21,11 @@ type
     Edit_Liczba: TEdit;
     Label1: TLabel;
     Label2: TLabel;
+    Label_ListBox: TLabel;
     ListBox_Cyfry: TListBox;
+    procedure Button_InfoClick(Sender: TObject);
     procedure Button_KoniecClick(Sender: TObject);
+    procedure Button_ListaClick(Sender: TObject);
     procedure Button_ObliczClick(Sender: TObject);
     procedure CheckGroup_OpcjeItemClick(Sender: TObject; Index: integer);
   private
@@ -45,6 +50,27 @@ begin
   Application.Terminate;
 end;
 
+procedure TForm_Main.Button_InfoClick(Sender: TObject);
+var
+  komunikat : string;
+begin
+  komunikat:='Autor: Marek, wersja programu: v.01';
+  Application.MessageBox(Pchar(komunikat),'Komunikat, hehe',MB_OK);
+end;
+
+procedure TForm_Main.Button_ListaClick(Sender: TObject);
+const cCyfry : array [1..5] of string = ('jeden', 'dwa', 'trzy', 'cztery', 'pięć');
+var
+  i : integer;
+begin
+  ListBox_Cyfry.Clear;
+
+  for i:=1 to 5 do
+      ListBox_Cyfry.Items.Add(IntToStr(i)+' - '+cCyfry[i]);
+  ListBox_Cyfry.Items.Add('Koniec listy');
+
+end;
+
 procedure TForm_Main.Button_ObliczClick(Sender: TObject);
 var
   komunikat : string;
@@ -53,8 +79,8 @@ begin
   try
     liczba:=StrToFloat(Edit_Liczba.Text);
   except
-    komunikat:='Blad konwersji liczby z lancucha: '+Edit_Liczba.Text;
-    Application.MessageBox(Pchar(Komunikat),'Blad',MB_OK);
+    komunikat:='Błąd konwersji liczby z łańcucha: '+Edit_Liczba.Text;
+    Application.MessageBox(Pchar(komunikat),'Błąd',MB_OK);
     Exit;
   end;
 
